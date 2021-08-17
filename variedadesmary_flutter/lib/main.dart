94fc47e0cd6b601e52f7030946d97fe998bbd957
David Cabrera <cabrerabuelvasd@gmail.com>
2021-08-17 17:12:05 -0500

    se esta trabajando en el appbar de flutter

--- variedadesmary_flutter/lib/main.dart
+++ variedadesmary_flutter/lib/main.dart
@@ -1,32 +1,18 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      theme: ThemeData(
-        primaryColor: Colors.lightBlue,
-      ),
+      title: 'Aplicacion',
+      theme: ThemeData(primaryColor: Colors.blue),
       debugShowCheckedModeBanner: false,
       home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green[300],
-      appBar: AppBar(
-        title: Text("VARIEDADES MARY"),
-      ),
-      body: Text("VARIEDADES MARY..."),
     );
   }
 }
