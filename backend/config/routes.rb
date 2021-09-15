7c7b2264084e4a67556c2d2c7e9f85dd5119ea0c
David Cabrera <cabrerabuelvasd@gmail.com>
2021-09-15 08:55:17 -0500

    se creo el inicio de sesion

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
+  root to: 'home#index'
+  devise_for :users
+
+  get 'devise/users'
+  # get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
