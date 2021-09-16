8c3200b2dd4bbb5c788a754d43fb33e2ff8fdc86
David Cabrera <cabrerabuelvasd@gmail.com>
2021-09-16 15:35:55 -0500

    Se creo la vista para crear productos

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-  get '/products', to: 'products#products'
+  # get '/products', to: 'products#products'
   root to: 'home#index'
   devise_for :users
 
   get 'devise/users'
 
+  # post '/products', to: 'products#create'
+
+  resources :products
 
   # get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
