046c88e3f8489b4211997b48bae9716288e416da
David Cabrera <cabrerabuelvasd@gmail.com>
2021-09-20 01:51:57 -0500

    cambios en los controladores y las vistas

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'vendors/index'
   # get '/products', to: 'products#products'
   root to: 'home#index'
   devise_for :users
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
 
   resources :products
 
+  resources :suppliers
   # get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
