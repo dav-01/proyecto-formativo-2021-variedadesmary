fe93a1ec12a923400fcbab3889f0670800954d8a
David Cabrera <cabrerabuelvasd@gmail.com>
2021-10-12 22:13:13 -0500

    cambios

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users
   get 'home/index'
 
   root to: 'home#index'
 
+  resources :products
+
+  resources :suppliers
+
+  resources :pqrs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
