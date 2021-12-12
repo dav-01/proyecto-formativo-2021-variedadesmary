d0c6ca4de4e30a0ca894b38f1cdb93bc56e111a3
David Cabrera <cabrerabuelvasd@gmail.com>
2021-12-12 15:45:46 -0500

    se agrego la edicion de usuario

--- plataforma/app/controllers/application_controller.rb
+++ plataforma/app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :telephone, :sex, :address, :birth_date])
+    devise_parameter_sanitizer.permit(:edit, keys: [:fullname, :telephone, :sex, :address, :birth_date])
   end
 
 
