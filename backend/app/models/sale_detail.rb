c7f077ff56897ac1e1b267988dc05c3e258a66fe
David Cabrera <cabrerabuelvasd@gmail.com>
2021-12-14 12:28:26 -0500

    Se elimino el proyecto backend que estaba incompleto y se dejo la plataforma web actual

--- backend/app/models/sale_detail.rb
+++
@@ -1,2 +0,0 @@
-class SaleDetail < ApplicationRecord
-end
--- backend/db/migrate/20211014132606_add_avatars_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAvatarsToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :products, :photo, :string
-  end
-end
